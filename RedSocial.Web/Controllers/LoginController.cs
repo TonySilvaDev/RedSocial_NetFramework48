b78392d01948778b3ec9ca44d1a128bc33ec8815
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-12-12 00:07:44 -0600

    Metodo registrar en login controller

--- RedSocial.Web/Controllers/LoginController.cs
+++ RedSocial.Web/Controllers/LoginController.cs
@@ -1,17 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using RedSocial.Entity;
+using RedSocial.Model;
 using System.Web.Mvc;
 
 namespace RedSocial.Web.Controllers
 {
     public class LoginController : Controller
     {
+        private UsuarioModel um = new UsuarioModel();
         // GET: Login
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult Registrar(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return Json(um.RegistroDeLogin(usuario));
+            }
+            else
+            {
+                return Json(new { response = false, message = "Ocurrió un error con la validación del formulario" });
+            }
         }
     }
 }
