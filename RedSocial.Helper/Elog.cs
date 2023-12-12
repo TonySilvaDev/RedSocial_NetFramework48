dc1247a10eac93ab9aab63b75409667767580d7c
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-12-12 00:03:36 -0600

    Se agrega Elog a helpers

---
+++ RedSocial.Helper/Elog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Web;
+
+namespace RedSocial.Helper
+{
+    public class ELog
+    {
+        public static void Save(object obj, Exception ex)
+        {
+            string fecha = DateTime.Now.ToString("yyyyMMdd");
+            string hora = DateTime.Now.ToString("HH:mm:ss");
+            string path = HttpContext.Current.Request.MapPath("~/log/" + fecha + ".txt");
+
+            StreamWriter sw = new StreamWriter(path, true);
+
+            StackTrace stacktrace = new StackTrace();
+            sw.WriteLine(obj.GetType().FullName + " " + hora);
+            sw.WriteLine(stacktrace.GetFrame(1).GetMethod().Name + " - " + ex.Message);
+            sw.WriteLine("");
+
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
