e93ecda491cf29e931bcae6c9e1deabe9df45670
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-12-14 00:05:00 -0600

    Panel lateral con info del usuario

---
+++ RedSocial.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using RedSocial.Helper;
+using RedSocial.Model;
+using System.Web.Mvc;
+
+namespace RedSocial.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioModel um = new UsuarioModel();
+
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ver(string name)
+        {
+            int id = ViewHelper.GetIdFromUrl(name);
+            return View(um.Obtener(id));
+        }
+    }
+}
