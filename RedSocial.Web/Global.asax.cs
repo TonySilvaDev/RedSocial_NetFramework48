b1e57a7c831d4968c05cd116c4a0f9c25e79ce87
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-12-23 21:44:23 -0600

    Pagina de errores

--- RedSocial.Web/Global.asax.cs
+++ RedSocial.Web/Global.asax.cs
@@ -1,6 +1,5 @@
+using RedSocial.Web.Controllers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -13,6 +12,29 @@ namespace RedSocial.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Server.ClearError();
+
+            RouteData routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            routeData.Values.Add("action", "Index");
+            routeData.Values.Add("exception", exception);
+
+            if (exception.GetType() == typeof(HttpException))
+            {
+                routeData.Values.Add("statusCode", ((HttpException)exception).GetHttpCode());
+            }
+            else
+            {
+                routeData.Values.Add("statusCode", 500);
+            }
+
+            IController controller = new ErrorController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
         }
     }
 }
